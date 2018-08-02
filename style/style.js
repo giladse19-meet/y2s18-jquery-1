edca41ba2067bfddc64432684b4c1e6c4c937f9d
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:08:56 +0300

    fun

--- style/style.js
+++ style/style.js
@@ -3,16 +3,16 @@
 
 
 function updateSize(newSize) {
-  // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
+  $('#logo').css('width',newSize) 
 }
 
 
 function updateLeftPosition(newX) {
-	// FINISH ME
+	$('#logo').css('left', newX)
 
 }
 
 function updateTopPosition(newY) {
-	// FINISH ME
+	$('#logo').css('top',newY)
 }
 
